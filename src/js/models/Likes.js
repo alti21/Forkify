b621706002487cd7fef51a914afb1732c4b9e282
alti21 <altemush@gmail.com>
2019-12-12 16:08:24 -0800

    Added functionality to like and unlike recipes

---
+++ src/js/models/Likes.js
@@ -0,0 +1,34 @@
+export default class Likes {
+    constructor() {
+        this.likes = [];
+    }
+
+    addLike(id, title, author, img) {
+        const like = {
+            id, 
+            title,
+            author,
+            img
+        }
+        this.likes.push(like);
+        return like;
+    }
+
+    deleteLike(id) {
+        const index = this.likes.findIndex(el => el.id === id);
+        //splices changes the original array
+        // [2,4,8] splice(1,1) (splice at index 1, splice 1 element) --> returns 4, original array is now [2,8]
+        // [2,4,8] splice(1,2) (splice at index 1, splice 2 elements) --> returns [4,8], original array is now 2
+        // [2,4,8] slice(1,2) (slice from index 1 to index 2) --> returns 4, original array is still [2,4,8]
+        this.likes.splice(index, 1);
+        //returns these elements and deletes them from the original array
+    }
+
+    isLiked(id) {
+        return this.likes.findIndex(el => el.id === id) !== -1;
+    }
+
+    getNumLikes() {
+        return this.likes.length;
+    }
+}
