744b0efc26118e9bf58d8a4438ac6ad4b2cb355a
alti21 <altemush@gmail.com>
2020-03-05 12:40:42 -0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,15 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = 
 {
+    //Entry point of webpack
     entry: ['@babel/polyfill','./src/js/index.js'],
     output: 
-    {
+    {   // Needs to be resolved to absolute path
         path: path.resolve(__dirname, 'dist'),
+        // bundled/compiled file name
         filename: 'js/bundle.js'
+        // for webpack dev server to identify the full path
+      //publicPath: '/dist'
     },
     devServer: 
     {
